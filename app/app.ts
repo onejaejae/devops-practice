96db9522b840c8ea3da2e208a299bbe1116d7430
wonjjee <yoteamo7@naver.com>
2023-10-21 17:22:15 +0900

    feat: added cpu intensive api for testing

--- app/app.ts
+++ app/app.ts
@@ -13,6 +13,17 @@ export const createApp = (client: RedisClient) => {
     res.status(200).send("hello from express!!!");
   });
 
+  const fibonacciFunc: any = (n: number) => {
+    if (n <= 1) return n;
+    return fibonacciFunc(n - 1) + fibonacciFunc(n - 2);
+  };
+
+  app.get("/fibonacci/:n", (req, res) => {
+    const n = parseInt(req.params.n, 10);
+    const result = fibonacciFunc(n);
+    res.send(`n: ${n} fibonacci result = ${result}`);
+  });
+
   app.get("/messages", async (req, res) => {
     const messages = await client.lRange(LIST_KEY, 0, -1);
     res.status(200).send(messages);
